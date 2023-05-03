fbc0d17c075ae944681aeb89ef6e95d19fe70640
Thiago Maciel <thiagoisoya@gmail.com>
2023-05-03 19:08:57 -0400

    If - Else em C++

---
+++ exercicios/aula007/if-else.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num1 = 10;
+    int num2 = 50;
+    char opc = 'S';
+
+    if(num1 != num2) {
+        cout << "Resultado verdadeiro";
+    } else {
+        cout << "Resultado falso";
+    }
+
+    return 0;
+}
