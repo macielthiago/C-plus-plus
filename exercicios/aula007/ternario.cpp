c970536cf319019ea5995aece3d9df432d30688d
Thiago Maciel <thiagoisoya@gmail.com>
2023-05-09 18:44:33 -0400

    Operador Ternário em C++

---
+++ exercicios/aula007/ternario.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int n1, n2, nota;
+    string res;
+    char opc;
+
+    inicio:
+    system ("cls");
+
+
+    cout << "Digite a primeira nota: ";
+    cin >> n1;
+    cout << "Digite a segunda nota: ";
+    cin >> n2;
+
+    nota = (n1 + n2) / 2; 
+
+    //(nota >= 60) ? res = "Aprovado"  : res = "reprovado";
+
+    res = (nota >= 60) ? "Aprovado"  : "Reprovado";
+    cout << "\n Situacao do aluno: " << res << "\n";
+
+    cout << "\nDigitar outras notas? [s / n]: ";
+    cin >> opc;
+
+    if (opc == 's' or opc == 'S') {
+        goto inicio;
+    }
+
+
+    system ("pause");
+    return 0;
+}
+
+/*
+(expressão)  ?  true   :   false;
+*/
